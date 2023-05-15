b0b114a4a624e95f5663aa4da4fa33faa76bc490
pugware <129339894+pugware@users.noreply.github.com>
2023-05-14 20:17:20 -0400

    Create honk2.js

---
+++ honk2.js
@@ -0,0 +1,40 @@
+(function(){
+    var img = document.createElement('img');
+    img.src = 'https://pugware.github.io/honk0in0honk/HONKimg/desktop-goose.png';
+    img.style.position = 'fixed';
+    img.style.bottom = '0px';
+    img.style.right = '0px';
+    img.style.zIndex = 9999;
+    img.setAttribute('draggable', false);
+    document.body.appendChild(img);
+    var speed = 5;
+    var direction = {x: -1, y: -1};
+    var chasing = false;
+    var chaseTimeout;
+    document.addEventListener('mousemove', function(e){
+        if(chasing){
+            var x = e.clientX;
+            var y = e.clientY;
+            img.style.right = (window.innerWidth - x - 50) + 'px';
+            img.style.bottom = (window.innerHeight - y - 50) + 'px';
+        }
+    });
+    img.addEventListener('click', function(){
+        chasing = true;
+        clearTimeout(chaseTimeout);
+        chaseTimeout = setTimeout(function(){
+            chasing = false;
+        }, 3000);
+    });
+    setInterval(function(){
+        if(!chasing){
+            var x = parseInt(img.style.right);
+            var y = parseInt(img.style.bottom);
+            if(x > window.innerWidth - 100 || x < 0) direction.x *= -1;
+            if(y > window.innerHeight - 100 || y < 0) direction.y *= -1;
+            img.style.right = (x + speed * direction.x) + 'px';
+            img.style.bottom = (y + speed * direction.y) + 'px';
+        }
+        img.style.transform = 'rotate(' + (Math.random() * 10 - 5) + 'deg)';
+    }, 20);
+})();
